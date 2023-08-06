a812bb181d822858e5bf9350472fb1911266a022
NQW1105 <neoqw@hotmail.com>
2023-08-06 15:30:12 +0800

    Completed task requirements

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.post('/new', (req, res, next) => {
+  const { messageText, messageUser } = req.body;
+  messages.push({ user: messageUser, text: messageText, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
